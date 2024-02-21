76f646cfdf81e6d53dadfc5db7cc92dca2844db8
arjun <arjun@DESKTOP-MBLJUFC>
2024-02-21 10:53:47 +0530

    Add project files.

---
+++ BookOrganiserWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookOrganiserWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookOrganiserWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options) : base(Options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
